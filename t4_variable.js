9d7f53d6eb96ffa5847951d60b3db489f1174fcb
Akhil <akhilskumar120@gmail.com>
2023-05-01 21:46:35 +0530

    Tutorial 4 added

---
+++ t4_variable.js
@@ -0,0 +1,14 @@
+let name = "Akhil";
+console.log(name);
+
+// Cannot use keywords
+// Variable must be meaningful
+// cannot start with a number
+// cannot contain space or hyphen
+
+let firstName = "Nikhil";
+console.log(firstName);
+
+let firstName1 = "Akhil";
+let lastName1 ="S Kumar";
+console.log(firstName1, lastName1)
