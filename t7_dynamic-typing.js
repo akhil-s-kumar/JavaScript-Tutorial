e0434baaa0083620ff5c92ab88b5c837e1514774
Akhil <akhilskumar120@gmail.com>
2023-05-08 22:51:27 +0530

    t7 added

---
+++ t7_dynamic-typing.js
@@ -0,0 +1,19 @@
+let name = "Akhil"; //String litrel
+let age = 22; // number litrel
+let isStudent = true; //Boolean Litrel
+let firstName = undefined;
+let selectedColor = null;
+
+console.log(typeof(age))
+age = false;
+console.log(typeof(age))
+
+console.log(typeof(selectedColor))
+
+let student = {
+    firstName: "Akhil",
+    lastName: "S Kumar",
+    age: 22,
+}
+
+console.log(typeof(student))
