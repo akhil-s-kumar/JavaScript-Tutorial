68e6fcd7a5c6f6d4049338b8db6c4be17e07faa7
Akhil <akhilskumar120@gmail.com>
2023-05-08 22:43:04 +0530

    t6 added

---
+++ t6_premitive_types.js
@@ -0,0 +1,11 @@
+let name = "Akhil"; //String litrel
+let age = 22; // number litrel
+let isStudent = true; //Boolean Litrel
+let firstName = undefined;
+let selectedColor = null;
+
+console.log(name)
+console.log(age)
+console.log(isStudent)
+console.log(firstName)
+console.log(selectedColor)
